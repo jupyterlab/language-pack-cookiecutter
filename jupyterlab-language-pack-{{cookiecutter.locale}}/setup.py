62598b0ac5324c1798374e0257fa316115b90be6
goanpeca <goanpeca@gmail.com>
2020-06-30 12:21:49 -0500

    Update license and copyright notice

--- jupyterlab-language-pack-{{cookiecutter.locale}}/setup.py
+++ jupyterlab-language-pack-{{cookiecutter.locale}}/setup.py
@@ -1,11 +1,28 @@
-from setuptools import find_packages, setup
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+ 
+from setuptools import setup
+{% set loc = cookiecutter.locale %}
 
 setup(
-    name="jupyterlab_language_pack_{{ cookiecutter.locale }}",
-    version='0.1.0',
-    url='https://github.com/goanpeca/jupyterlab-language-packs',
-    description='Jupyterlab {{ cookiecutter.language }} Language Pack',
-    install_requires=["jupyterlab_language==0.1.0"],
-    packages=find_packages(),
-    entry_points={'jupyterlab.languagepacks': '{{ cookiecutter.locale }} = jupyterlab_language_pack_{{ cookiecutter.locale }}'},
+    name="jupyterlab_language_pack_{{loc}}",
+    version="0.1.0",
+    url="https://github.com/goanpeca/jupyterlab-language-packs",
+    description="Jupyterlab {{ cookiecutter.language }} Language Pack",
+    install_requires=[],
+    packages=[
+        "jupyterlab_language_pack_{{loc}}",
+        "jupyterlab_language_pack_{{loc}}.extensions",
+    ],
+    package_dir={
+        "jupyterlab_language_pack_{{loc}}": "jupyterlab_language_pack_{{loc}}",
+        "jupyterlab_language_pack_{{loc}}.extensions": "jupyterlab_language_pack_{{loc}}/extensions",
+    },
+    package_data={
+        "jupyterlab_language_pack_{{loc}}": ["*.json", "*.mo"],
+        "jupyterlab_language_pack_{{loc}}.extensions": ["*.json", "*.mo",],
+    },
+    entry_points={
+        "jupyterlab.languagepack": ["{{loc}} = jupyterlab_language_pack_{{loc}}",]
+    },
 )
