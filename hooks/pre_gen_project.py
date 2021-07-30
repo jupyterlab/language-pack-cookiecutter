d08647e5b6dea0d8f80e9cfa22cc8cc81911442d
Frédéric Collonval <fcollonval@gmail.com>
2021-07-30 10:09:09 +0200

    Use gettext structure https://docs.python.org/3/library/gettext.html#gettext.bindtextdomain

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -5,35 +5,36 @@ import sys
 
 from babel import Locale
 
-locale = "{{ cookiecutter.locale }}"
-locale_underscore = "{{ cookiecutter.locale_underscore }}"
+if __name__ == "__main__":
+    locale = "{{ cookiecutter.locale }}"
+    locale_underscore = "{{ cookiecutter.locale_underscore }}"
 
 
-if "-" not in locale:
-    print(
-        "ERROR: `locale` must be 4 letter code, like es-ES.\n"
-        "       See http://www.lingoes.net/en/translator/langcode.htm"
-    )
-    sys.exit(1)
+    if "-" not in locale:
+        print(
+            "ERROR: `locale` must be 4 letter code, like es-ES.\n"
+            "       See http://www.lingoes.net/en/translator/langcode.htm"
+        )
+        sys.exit(1)
 
 
-try:
-    babel_locale = Locale.parse(locale_underscore)
-    parsed_locale = babel_locale.language + "_" + babel_locale.territory
-    # There is one exception for Norwegian
-    if parsed_locale != locale_underscore and (parsed_locale != 'nb_NO' or locale_underscore != 'no_NO'):
+    try:
+        babel_locale = Locale.parse(locale_underscore)
+        parsed_locale = babel_locale.language + "_" + babel_locale.territory
+        # There is one exception for Norwegian
+        if parsed_locale != locale_underscore and (parsed_locale != 'nb_NO' or locale_underscore != 'no_NO'):
+            print('ERROR: Locale "{0}" is invalid!'.format(locale))
+            sys.exit(1)
+    except Exception:
         print('ERROR: Locale "{0}" is invalid!'.format(locale))
         sys.exit(1)
-except Exception:
-    print('ERROR: Locale "{0}" is invalid!'.format(locale))
-    sys.exit(1)
 
 
-if "_" in locale:
-    print("ERROR: Locales with language variants must use `-` and not `_`")
-    sys.exit(1)
+    if "_" in locale:
+        print("ERROR: Locales with language variants must use `-` and not `_`")
+        sys.exit(1)
 
 
-if "-" in locale_underscore:
-    print("ERROR: `locale_underscore` must use `_` and not `-`")
-    sys.exit(1)
+    if "-" in locale_underscore:
+        print("ERROR: `locale_underscore` must use `_` and not `-`")
+        sys.exit(1)
